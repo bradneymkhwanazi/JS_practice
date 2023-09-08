22a151a83351bee218dfffae41d35516b4face82
Bradney Mkhwanazi <87267705+bradneymkhwanazi@users.noreply.github.com>
2023-09-08 13:55:36 +0200

    Add files via upload

---
+++ JS practical.js
@@ -0,0 +1,10 @@
+/* [Console]
+The console is a panel that displays important messages, 
+like errors, for developers. 
+Much of the work the computer does with our code is invisible to us by default. 
+If we want to see things appear on our screen, we can print, or log, 
+to our console directly.
+*/
+console.log(99);
+
+/*
