5ab1043db4a236348ad7cd394d20ce732f162bd1
Bradney Mkhwanazi <87267705+bradneymkhwanazi@users.noreply.github.com>
2023-09-11 18:52:27 +0200

    buildup

--- JS practical.js
+++ JS practical.js
@@ -7,4 +7,18 @@ to our console directly.
 */
 console.log(99);
 
-/*
+/* Data Types : are the classifications we give to the 
+different kinds of data that we use in programming.
+
+* Numbers: Any number including numbers with decimals
+* BigInt: any number greater 2 exp 53
+* String: Any grouping of characters on the keyboard
+* Boolean: This data type only has two possible values either true or false
+* Null: this data type represents the intentional absence of a value. This is represented 
+by the keyword null without quotes
+* Undefined: this data type is denoted by the keyword undefined
+* Symbol : A newer feauture to the language, symbols are unique identifiers
+useful in more complex coding
+ * Object: collections of related data.
+
+The first 6 of those types are considered primitive data types.
